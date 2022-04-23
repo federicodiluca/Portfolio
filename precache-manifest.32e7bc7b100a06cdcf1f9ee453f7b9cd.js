200107e6016a58e22b6b0542782e5e170742dfc9
Federico Di Luca <federico.diluca95@gmail.com>
2022-04-23 20:06:19 +0200

    Updates

---
+++ precache-manifest.32e7bc7b100a06cdcf1f9ee453f7b9cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5869b15558b3c969e04f0600ebf02910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f0c9219880deae93c1",
+    "url": "/static/css/main.5629a331.chunk.css"
+  },
+  {
+    "revision": "40224c97c7adf8886fa8",
+    "url": "/static/js/2.00607ce4.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.00607ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f0c9219880deae93c1",
+    "url": "/static/js/main.a0749260.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "df9c0f847ae753f59694093f568e55cd",
+    "url": "/static/media/FeDL95.df9c0f84.jpg"
+  },
+  {
+    "revision": "4ca66bb0ea59e64fca03c686ef1128dd",
+    "url": "/static/media/resume.4ca66bb0.pdf"
+  }
+]);
