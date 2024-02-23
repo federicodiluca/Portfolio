a1e7dd9a5af94043eeafb8a8b9aeb8640ec76a34
Federico Di Luca <68862675+federicodiluca@users.noreply.github.com>
2024-02-23 11:42:23 +0100

    update my-sql icon

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -125,7 +125,7 @@ const skills = {
     { url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg", alt:"javascript" },
     { url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-plain.svg", alt:"jquery" },
     { url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", alt:"react" },
-    { url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg", alt:"my-sql" },
+    { url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg", alt:"my-sql" },
     { url: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg", alt:"postman" },
     { url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg", alt:"docker" },
     { url: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg", alt:"python" },
